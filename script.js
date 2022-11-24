013606920dc8c9cb00bad1f4d4f1c885e90d7078
Diogo <diogorodrigueslife@gmail.com>
2022-11-24 15:51:08 -0300

    :rocket: renomeando objetos

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const imgs = [
     {'id' : '1', 'url': './img/calculadora_com_react.jpg'},
     {'id' : '2', 'url': './img/todolista_com_javascript.png'},
@@ -17,18 +16,18 @@ const  loadImgs = (imgs, conteiner)=>{
 
 loadImgs( imgs, conteinerItens);
 
-let itens = document.querySelectorAll('.item')
+let listaDeItens = document.querySelectorAll('.item')
 
-const previous = () =>{
-    conteinerItens.appendChild( itens[0])
-    itens = document.querySelectorAll('.item')
+const slideAnterior = () =>{
+    conteinerItens.appendChild( listaDeItens[0])
+    listaDeItens = document.querySelectorAll('.item')
 }
 
-const next = () =>{
-    const lastItem = itens[itens.length -1]
-    conteinerItens.insertBefore( lastItem, itens[0])
-    itens = document.querySelectorAll('.item')
+const proximoSlide = () =>{
+    const lastItem = listaDeItens[listaDeItens.length -1]
+    conteinerItens.insertBefore( lastItem, listaDeItens[0])
+    listaDeItens = document.querySelectorAll('.item')
 }
 
-document.querySelector('#previous').addEventListener('click', previous)
-document.querySelector('#next').addEventListener('click', next)
+document.querySelector('#previous').addEventListener('click', slideAnterior)
+document.querySelector('#next').addEventListener('click', proximoSlide)
