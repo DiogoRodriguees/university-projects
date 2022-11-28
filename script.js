0c11b0841977fb4c3214554a18ad0324659bff53
DiogoRodriguees <diogorodrigueslife@gmail.com>
2022-11-27 23:38:00 -0300

    :rocket: Nova sessão comslide show

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 const imgs = [
-    {'id' : '3', 'url': './img/slideshow/calculadora_com_react.jpg'},
-    {'id' : '2', 'url': './img/slideshow/img_projetofinancas.png'},
-    {'id' : '1', 'url': './img/slideshow/todolista_com_javascript.png'},
+    {'id' : '2', 'url': './img/meusprojetos/Captura de Tela (33).png'},
+    {'id' : '5', 'url': './img/meusprojetos/Captura de Tela (31).png'},
+    {'id' : '3', 'url': './img/meusprojetos/Captura de Tela (31).png'},
+    {'id' : '4', 'url': './img/meusprojetos/Captura de Tela (32).png'},
+    // {'id' : '1', 'url': './img/meusprojetos/Captura de Tela (34).png'},
 ]
 
 const conteinerItens = document.querySelector('#conteiner')
 
 const  loadImgs = (imgs, conteiner)=>{
     imgs.forEach(img => {
-        conteiner.innerHTML +=
-        `<div class='item'>
-        <img src='${img.url}'>
-        </div>`
+
+        if(img.id === '5'){
+            conteiner.innerHTML +=
+            `<div class='item'>
+                <p>Arraste para o lado para conferir as imagens do projeto</p>
+                <img src='${img.url}'>
+            </div>`
+        }else{
+            conteiner.innerHTML +=
+            `<div class='item'>
+            <img src='${img.url}'>
+            </div>`
+        }
     });
 }
 
@@ -19,16 +30,16 @@ loadImgs( imgs, conteinerItens);
 
 let listaDeItens = document.querySelectorAll('.item')
 
-const proximoSlide = () =>{
+const nextSlide = () =>{
     conteinerItens.appendChild( listaDeItens[0])
     listaDeItens = document.querySelectorAll('.item')
 }
 
-const slideAnterior = () =>{
+const previousSlide = () =>{
     const lastItem = listaDeItens[listaDeItens.length -1]
     conteinerItens.insertBefore( lastItem, listaDeItens[0])
     listaDeItens = document.querySelectorAll('.item')
 }
 
-document.querySelector('#previous').addEventListener('click', slideAnterior)
-document.querySelector('#next').addEventListener('click', proximoSlide)
+document.querySelector('#previous').addEventListener('click', previousSlide)
+document.querySelector('#next').addEventListener('click', nextSlide)
