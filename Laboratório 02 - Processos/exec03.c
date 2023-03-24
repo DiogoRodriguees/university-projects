26f61caf427ccf90ff05f14983e69edd1d9415cc
Marcos Vinicius de Quadros <98984018+MarcosQuadross@users.noreply.github.com>
2023-03-23 21:14:38 -0300

    update exec03.c

---
+++ Laboratório 02 - Processos/exec03.c
@@ -0,0 +1,58 @@
+#include <stdio.h>     // printf()
+#include <stdlib.h>    // exit()
+#include <unistd.h>    // fork()
+#include <sys/types.h> // pid_t
+#include <stdbool.h>
+
+int main()
+{
+    int quantidadeProcessos = 3;
+    int vetor[] = {0, 7, 2, 3, 4, 5, 6, 7, 8, 9};
+    int tamanhoVetor = sizeof(vetor) / sizeof(vetor[0]);
+    int intervaloProcesso = tamanhoVetor / quantidadeProcessos;
+    int sobraVetor = tamanhoVetor % quantidadeProcessos;
+    int posInicial, posFinal;
+    int valorBuscado = 7;
+    int ultimoPid = 0;                  // guarda o PID do último processo filho que fez a busca
+
+    
+    // printf("pstree -c -p %d \n\n", getpid());
+    pid_t pid;
+    pid = fork();
+
+    // Loop responsável por criar os processos filhos
+    for (int i = 0; i < quantidadeProcessos; i++)
+    {
+        if (pid && i != 0)
+        {
+            pid = fork();
+        
+        } 
+        
+        if (pid == 0 && ultimoPid != getpid()) {
+            ultimoPid = getpid();
+            
+            // Define a primeira posição do processo
+            int posInicial = i * intervaloProcesso;
+
+            // Define a última posição que o processo irá buscar
+            if(i == quantidadeProcessos -1) {
+                posFinal = tamanhoVetor;
+            } else {
+                posFinal = (i+1) * intervaloProcesso; 
+            }
+            
+            // Busca o valor procurado no vetor e exibe o pid do processo filho
+            for (int j = posInicial; j < posFinal; j++) {
+                if(vetor[j] == valorBuscado)
+                {
+                    printf("Processo filho PID: %d, encontrou o valor!\n", getpid());
+                }
+            }
+
+        }
+    }
+
+    getchar();
+    exit(0);
+}
