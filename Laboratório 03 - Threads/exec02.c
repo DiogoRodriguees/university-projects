50001b0544f7a545ed6e654464e06adbbb11cf32
diogorodriguees <diogorodrigueslife@gmail.com>
2023-03-28 22:47:59 -0300

    :rocket: reorganizando e comentando codigo

--- Laboratório 03 - Threads/exec02.c
+++ Laboratório 03 - Threads/exec02.c
@@ -9,6 +9,7 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <math.h>
 #include "matriz.h"
 
@@ -20,42 +21,60 @@ typedef struct matrizDate
     int **matriz;
 } matrizDate;
 
+/* estrutura que guarda os dados para utilizar no calculo da media aritimetica */
+typedef struct aritmethicDates
+{
+    int line;
+    matrizDate *dates;
+} aritmethicDates;
+
 float vetor_respostas[99];
 
-void writeMatrizInFile(int **, int, int);
-int **newMatriz(int, int);
-matrizDate *newMatrizDate(int, int);
-void createThreads(int size);
-void copyDatesForMatriz(matrizDate*);
+/* cria uma matriz com valores aleatorios */
+int **newMatriz(int, int); 
+
+/* cria uma estrutura de matrizDate */
+matrizDate *newMatrizDate(int, int); 
+
+/* escreve os dados da matriz no arquivo de saida */
+void writeMatrizInFile(int **, int, int); 
+
+/* copia os dados de uma entrada para um determinada matriz */
+void copyDatesForMatriz(matrizDate *);    
+
+/* realiza o calculo da media aritimetica da linhas */
+void *aritmethicCalculate(void *);        
 
 int main(int argc, char **argv)
 {
-    if (argc < 4)
-    {
-        printf("É necessário informar o numero de linhas e colunas da matriz, e o numero de threads!\n");
+    if (validateInput(argc))
         return 0;
-    }
 
-    int rows = atoi(argv[1]);
-    int columns = atoi(argv[2]);
-    int threadsAmout = atoi(argv[3]);
+    int rows = atoi(argv[1]);         // numero de linhas da matriz
+    int columns = atoi(argv[2]);      // numero de colunas da matriz
+    int threadsAmout = atoi(argv[3]); // quantidade de threads que serão criadas
 
-    int **matriz = newMatriz(rows, columns);
-    writeMatrizInFile(matriz, rows, columns);
+    int **matriz = newMatriz(rows, columns);  // iniciando uma matriz vazia
+    writeMatrizInFile(matriz, rows, columns); // preenchendo a matriz criada com os valores do arquivo de entrada
 
-    pthread_t threadsRows[threadsAmout]; // threads para calcular média aritimetica
+    pthread_t threadsRows[threadsAmout];    // threads para calcular média aritimetica
     pthread_t threadsColumns[threadsAmout]; // threads para calcular média geometrica
 
     matrizDate *dateMatriz = newMatrizDate(rows, columns); // iniciando uma estrutura para guardar os dados
-    copyDatesForMatriz(dateMatriz); // copiando os dados para a nova estrutura
+    copyDatesForMatriz(dateMatriz);                        // copiando os dados para a nova estrutura
 
-    
+    print_matrix(dateMatriz->matriz, rows, columns); // printando a matriz para conferência
 
-    print_matrix(dateMatriz->matriz, rows, columns);
-
+    /* criando as threads para calcular media aritimetica */
     for (int i = 0; i < threadsAmout; i++)
     {
-        // pthread_create(&threadsRows[i], NULL, aritmethicCalculate, 1);
+        /* criando estrutra e preenchendo os dados */
+        aritmethicDates *dates = malloc(sizeof(aritmethicDates));
+        dates->line = i;
+        dates->dates = dateMatriz;
+
+        /* criação das threads */
+        pthread_create(&threadsRows[i], NULL, aritmethicCalculate, dates);
     }
 
     return 0;
@@ -84,26 +103,33 @@ matrizDate *newMatrizDate(int rows, int columns)
     return newMatriz;
 }
 
-void copyDatesForMatriz(matrizDate * dates){
-    FILE *arqOrigem = fopen("matrizTexto.in", "r");
+void copyDatesForMatriz(matrizDate *dates)
+{
+    FILE *arqOrigem = fopen("matrizTexto.in", "r"); // abrindo arquivo para leitura
+
     int i = 0;
     int j = 0;
+    int number = 0; // variavel qu guarda o valor lido no arquivo
 
-    int number = 0;
     while (i < dates->r)
     {
+        /* lendo o arquivo */
         fscanf(arqOrigem, "%i", &number);
+
+        /* escreve o valor lido na linha indexada pelo i */
         if (j < dates->c)
         {
             dates->matriz[i][j++] = number;
         }
+
+        /* troca o indexador i quando o numero maximo de colunas é atingido */
         if (j == dates->c)
         {
             j = 0;
             i++;
         }
     }
-    fclose(arqOrigem);
+    fclose(arqOrigem); // fecha o arquivo leitura
 }
 
 void writeMatrizInFile(int **matriz, int rows, int columns)
@@ -124,27 +150,36 @@ void writeMatrizInFile(int **matriz, int rows, int columns)
     fclose(arqMatriz);
 }
 
-// void *aritmethicCalculate(int rows, int columns)
-// {
+void *aritmethicCalculate(void *dates)
+{
+    aritmethicDates *localdates = dates;
 
-//     float media, x, soma;
-//     media = x = soma = 0;
+    float media, x, soma;
+    media = x = soma = 0;
 
-//     int **matriz = fopen(arqLeitura, "r");
+    for (int i = 0; i <= localdates->dates->r; i++)
+    {
+        for (int j = 0; j < localdates->dates->c; j++)
+        {
+            if (i == localdates->line)
+            {
+                soma += localdates->dates->matriz[i][j];
+            }
+        }
+    }
 
-//     for (int i = 0; i <= rows; i++)
-//     {
-//         for (int j = 0; j < columns; j++)
-//         {
-//             if (i == linha)
-//             {
-//                 soma += matriz[i][j];
-//             }
-//         }
-//     }
+    media = soma / localdates->dates->c;
 
-//     media = soma / columns;
+    printf("%f", soma);
+    return NULL;
+}
 
-//     printf("%i", soma);
-//     return NULL;
-// }
+bool validateInput(int number)
+{
+    if (number < 4)
+    {
+        printf("É necessário informar o numero de linhas e colunas da matriz, e o numero de threads!\n");
+        return false;
+    }
+    return true;
+}
