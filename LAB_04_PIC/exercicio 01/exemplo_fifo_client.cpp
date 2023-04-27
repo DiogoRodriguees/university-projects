78efbcf87e2485872bfa24a964ccf7f0148c7e18
diogorodriguees <diogorodrigueslife@gmail.com>
2023-04-27 10:53:42 -0300

    :rocket: exemplos dos slides

---
+++ LAB_04_PIC/exercicio 01/exemplo_fifo_client.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+
+#define SERVER_FIFO "/tmp/serverfifo"
+
+int main()
+{
+    exit(0);
+}
---
+++ LAB_04_PIC/exercicio 01/exemplo_fifo_server.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+#define SERVER_FIFO "/tmp/serverfifo"
+
+#define N 5
+
+int main(int argc, char** argv)
+{
+    int fd_server; // descritor para o fifo
+    
+    
+    
+    exit(0);
+}
