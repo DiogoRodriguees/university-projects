e1859281d36572fb2373386cb38cf61ba7281bc0
diogorodriguees <diogorodrigueslife@gmail.com>
2023-04-28 22:35:26 -0300

    :rocket: atualizando

--- Laboratório 05 - IPCs/exemplos dos slides/exemplo_fifo_client.cpp
+++ Laboratório 05 - IPCs/exemplos dos slides/exemplo_fifo_client.cpp
@@ -8,9 +8,51 @@
 #include <string.h>
 
 #define SERVER_FIFO "/tmp/serverfifo"
-#define N 5
 
-int main()
+int main(int argc, char **argv)
 {
+    int fd_server, num_bytes_read; // descritor para o fifo
+    char buf[512];
+
+    /* cria um FIFO quando ele não exista */
+    if ((mkfifo(SERVER_FIFO, 0664) == -1) && (errno != EEXIST))
+    {
+        perror("mkfifo");
+        exit(1);
+    }
+
+    /* abre o fifo */
+    if ((fd_server = open(SERVER_FIFO, O_RDONLY)) == -1)
+    {
+        perror("open");
+    }
+
+    /* lê e trata as mensagens do FIFO */
+    while (1)
+    {
+        memset(buf, '\0', sizeof(buf));
+        num_bytes_read = read(fd_server, buf, sizeof(buf));
+        switch (num_bytes_read)
+        {
+        case -1:
+            perror("-- read error -- ");
+            break;
+
+        case 0:
+            printf("-- None data ... close and reopen fifo --\n");
+            close(fd_server);
+            fd_server = open(SERVER_FIFO, O_RDONLY);
+            break;
+
+        default:
+            printf("Received %d bytes: %s\n", num_bytes_read, buf);
+        }
+
+        if (close(fd_server) == -1)
+        {
+            perror("close");
+        }
+    }
+
     exit(0);
 }
--- Laboratório 05 - IPCs/exemplos dos slides/exemplo_fifo_server.cpp
+++ Laboratório 05 - IPCs/exemplos dos slides/exemplo_fifo_server.cpp
@@ -8,51 +8,37 @@
 #include <string.h>
 
 #define SERVER_FIFO "/tmp/serverfifo"
+#define N 5
+char buf[512];
 
 int main(int argc, char **argv)
 {
-    int fd_server, num_bytes_read; // descritor para o fifo
-    char buf[512];
-
-    /* cria um FIFO quando ele não exista */
-    if ((mkfifo(SERVER_FIFO, 0664) == -1) && (errno != EEXIST))
-    {
-        perror("mkfifo");
-        exit(1);
+    int fd_server; // descritor do FIFO
+    
+    if( (fd_server = open (SERVER_FIFO, O_WRONLY)) == -1){
+        perror("open error: server fifo");
+        return 1;
     }
-
-    /* abre o fifo */
-    if ((fd_server = open(SERVER_FIFO, O_RDONLY)) == -1)
-    {
-        perror("open");
+    
+    int value = 0;
+    
+    while(value < N){
+        // cria mensagens incrementalmente
+        sprintf(buf, "Menssage %ld - Number %d", (long) getpid(), value++);
+        printf("Sending: %s\n", buf);
+        
+        // envia a mensagem para o fifo
+        write(fd_server, buf, strlen(buf));
+        
+        // somente para vizualização
+        sleep(1);
+        
     }
-
-    /* lê e trata as mensagens do FIFO */
-    while (1)
-    {
-        memset(buf, '\0', sizeof(buf));
-        num_bytes_read = read(fd_server, buf, sizeof(buf));
-        switch (num_bytes_read)
-        {
-        case -1:
-            perror("-- read error -- ");
-            break;
-
-        case 0:
-            printf("-- None data ... close and reopen fifo --\n");
-            close(fd_server);
-            fd_server = open(SERVER_FIFO, O_RDONLY);
-            break;
-
-        default:
-            printf("Received %d bytes: %s\n", num_bytes_read, buf);
-        }
-
-        if (close(fd_server) == -1)
-        {
-            perror("close");
-        }
+    
+    if(close (fd_server) == -1) {
+        perror("close error: server fifo");
+        return 2;
     }
-
+    
     exit(0);
 }
