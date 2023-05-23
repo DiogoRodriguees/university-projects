fe2f34688dfb46d1853fc6d4dc740b221a8cfff5
diogorodriguees <diogorodrigueslife@gmail.com>
2023-05-23 18:17:02 -0300

    :rocket: atualizando

---
+++ PROJETO_1/main.c
@@ -0,0 +1,34 @@
+#define MAX_ALUNOS_GRUPO 10
+#define MAX_MONITORES_GRUPO 1
+#define MAX_TURMA
+
+/* 
+    Ações dos PROFESSORES
+        * abrirSala
+        * fecharSala
+        * avisarAlunos
+        * avisarMonitores
+    
+    Ações dos ALUNOS
+        * entraSala
+        * sairSala
+        * estudar
+         
+    Ações dos MONITORES
+        * entrarSala
+        * sairSala
+        * supervisionarAlunos
+    
+    OBS:
+        Exibir uma mensagem para cada ação executada.
+*/
+
+
+
+int main()
+{
+    /* Criar threads para: Alunos, Monitores, Professores */
+    
+    
+    return 0;
+}
