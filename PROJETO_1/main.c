d143e78b1765f712dc5038df980ade54961f06c6
diogorodriguees <diogorodrigueslife@gmail.com>
2023-05-23 18:36:13 -0300

    :rocket: iniciando a estrutura do codigo

--- PROJETO_1/main.c
+++ PROJETO_1/main.c
@@ -1,34 +1,38 @@
+#include <stdbool.h> // bool
+
+/* Variaveis para teste com diferentes numeros de ALUNOS, MONITORES e PROFESSORES */
 #define MAX_ALUNOS_GRUPO 10
 #define MAX_MONITORES_GRUPO 1
-#define MAX_TURMA
+#define MAX_TURMA_ALUNOS 30
 
-/* 
+/* Variaveis que serão inicializadas pelo monitor */
+int estudantesSala;   // numero de estudantes na sala
+bool grupoTemMonitor; // verifica se o grupo possui um monitor
+
+/*
     Ações dos PROFESSORES
         * abrirSala
         * fecharSala
         * avisarAlunos
         * avisarMonitores
-    
+
     Ações dos ALUNOS
         * entraSala
         * sairSala
         * estudar
-         
+
     Ações dos MONITORES
         * entrarSala
         * sairSala
         * supervisionarAlunos
-    
+
     OBS:
         Exibir uma mensagem para cada ação executada.
 */
 
-
-
 int main()
 {
     /* Criar threads para: Alunos, Monitores, Professores */
-    
-    
+
     return 0;
 }
