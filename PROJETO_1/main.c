52ed698576a8e680ce560a6d1a2371e82474099f
diogorodriguees <diogorodrigueslife@gmail.com>
2023-05-24 15:05:48 -0300

    :rocket: novas bibliotecas importadas

--- PROJETO_1/main.c
+++ PROJETO_1/main.c
@@ -2,14 +2,13 @@
     Autores:
         Diogo Rodrigues dos Santos - 2380242
         Marcos Vinicios de Quadros - 2380560
-    
+
     Descrição:
-        
-        
+
+
     Data:
         08 de Junho de 2023
 */
-
 /*
     Ações dos PROFESSORES
         * abrirSala
@@ -31,8 +30,10 @@
         Exibir uma mensagem para cada ação executada.
 */
 
-#include <stdbool.h> // bool
-#include <pthread.h> // pthread_t
+#include <stdbool.h>   // bool
+#include <pthread.h>   // pthread_t
+#include <semaphore.h> // sem_t
+#include <stdio.h>
 
 /* Variaveis para teste com diferentes numeros de ALUNOS, MONITORES e PROFESSORES */
 #define MAX_ALUNOS_GRUPO 10
@@ -42,9 +43,10 @@
 /* Variaveis que serão inicializadas pelo monitor */
 int estudantesSala;   // numero de estudantes na sala
 bool grupoTemMonitor; // verifica se o grupo possui um monitor
+int buffer;           // buffer iniciado como INT apenas para teste
+sem_t mutex;          // semaforo
 
-
-int main()
+int main(int argc, char **argv)
 {
     /* Criar threads para: Alunos, Monitores, Professores */
     pthread_t alunos, professores, monitores;
