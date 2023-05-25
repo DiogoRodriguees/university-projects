026bfc5ec906f6e2d34a6112fbd43da9e908a2ba
diogorodriguees <diogorodrigueslife@gmail.com>
2023-05-25 13:56:49 -0300

    :rocket: atualziando

--- PROJETO_1/main.c
+++ PROJETO_1/main.c
@@ -34,22 +34,72 @@
 #include <pthread.h>   // pthread_t
 #include <semaphore.h> // sem_t
 #include <stdio.h>
+#include <unistd.h>
 
 /* Variaveis para teste com diferentes numeros de ALUNOS, MONITORES e PROFESSORES */
-#define MAX_ALUNOS_GRUPO 10
+#define MAX_ALUNOS_GRUPO 5
+#define MAX_TURMA_ALUNOS 5
 #define MAX_MONITORES_GRUPO 1
-#define MAX_TURMA_ALUNOS 30
 
 /* Variaveis que serão inicializadas pelo monitor */
-int estudantesSala;   // numero de estudantes na sala
-bool grupoTemMonitor; // verifica se o grupo possui um monitor
-int buffer;           // buffer iniciado como INT apenas para teste
-sem_t mutex;          // semaforo
+int total_alunos = 0;
+
+sem_t s_alunos;    // semaforo para controle do grupo
+sem_t s_monitores; // semaforo para controle do grupo
+int monitores_disponiveis = 0;
+
+void *executarAlunos(void *id)
+{
+    int a_id = *((int *)id);
+    sem_wait(&s_monitores);
+    printf("ALUNO %i ENTROU NA SALA\n", a_id);
+}
+
+void *executarMonitores(void *id)
+{
+    int m_id = *((int *)id);
+    sem_post(&s_monitores);
+    printf("MONITOR %i ENTROU NA SALA\n", m_id);
+}
 
 int main(int argc, char **argv)
 {
     /* Criar threads para: Alunos, Monitores, Professores */
-    pthread_t alunos, professores, monitores;
+    pthread_t alunos[MAX_TURMA_ALUNOS], monitores[MAX_MONITORES_GRUPO], professor;
+
+    /* Criação do semaforo para os ALUNOS */
+    sem_init(&s_alunos, 0, MAX_TURMA_ALUNOS);
+
+    /* Semaforos dos MONITORES*/
+    sem_init(&s_monitores, 0, MAX_MONITORES_GRUPO);
+
+    /* Criando alunos */
+    for (int i = 0; i < MAX_TURMA_ALUNOS; i++)
+    {
+        pthread_create(&alunos[i], NULL, executarAlunos, &i);
+        sleep(1);
+    }
+
+    /* Criando monitores */
+    for (int i = 0; i < MAX_MONITORES_GRUPO; i++)
+    {
+        pthread_create(&monitores[i], NULL, executarMonitores, &i);
+        sleep(1);
+    }
+
+    /* Aguardando alunos */
+    for (int i = 0; i < MAX_TURMA_ALUNOS; i++)
+        pthread_join(alunos[i], NULL);
+
+    /* Aguardando monitores */
+    for (int i = 0; i < MAX_MONITORES_GRUPO; i++)
+        pthread_join(monitores[i], NULL);
+
+    /* Semaforos dos MONITORES*/
+    sem_destroy(&s_monitores);
+    
+    /* Criação do semaforo para os ALUNOS */
+    sem_destroy(&s_alunos);
 
     return 0;
 }
