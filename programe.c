68c8f62fe9e9236aaa248e08885108054f17f8c9
diogorodriguees <diogorodrigueslife@gmail.com>
2023-06-14 18:33:57 -0300

    :rocket: cabaçalho  sem descrição  adicionado

---
+++ programe.c
@@ -0,0 +1,16 @@
+/* 
+    Autores:
+        Diogo Rodrigues dos Santos - 2380242
+        Marcos Vinicius de Quadros - 2380560
+    
+    Descrição:
+        
+    Data:
+        04 de Julho 2023 
+*/
+
+int main(int argc, char **argv)
+{
+
+  return 0;
+}
