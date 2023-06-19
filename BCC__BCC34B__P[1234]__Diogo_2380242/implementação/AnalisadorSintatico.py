a0009d68375baa1d1608ebda7490256da1ec8fdc
diogorodriguees <diogorodrigueslife@gmail.com>
2023-06-18 21:31:48 -0300

    :rocket: parser inicializado

---
+++ BCC__BCC34B__P[1234]__Diogo_2380242/implementação/AnalisadorSintatico.py
@@ -0,0 +1,40 @@
+from automata.pda.npda import NPDA
+from sys import argv, exit
+
+# verifica se a quantidade de parametros esta certa
+if(len(argv) != 2):
+        print("Insira uma expressão")
+        exit()
+        
+# recebendo a expressão da entrada        
+entrada = argv[1]
+
+npda = NPDA(
+    # estados
+    states={'INITIAL_STATE', 'FINAL_STATE'},
+    
+    # alfabeto de entrada
+    input_symbols={},
+    
+    # transições
+    stack_symbols={},
+    
+    # estado inicial
+    initial_state='INITIAL_STATE',
+    
+    # estado inicial da pilha
+    initial_stack_symbol='0',
+    
+    # estado final
+    final_states={'EXPRESSAO_ACEIT'},
+    
+    # modulo atingido pelo estado "aceito"
+    acceptance_mode='FINAL_STATE'
+    
+)
+
+if npda.acceptance_mode(entrada + "$"):
+    print(f"Expressao {entrada} : Aceita!")
+else:
+    print(f"Expressao {entrada} : Rejeitada!")
+
