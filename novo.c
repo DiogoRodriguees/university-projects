4699f21a2a93db21a6c1ab0b6c4f1ad0abbcbe66
Marcos Quadros <marcosdquadros@gmail.com>
2023-06-21 20:21:05 -0300

    atualizando

--- novo.c
+++ novo.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
 // Estrutura para a tabela de páginas
 typedef struct {
     int pageNumber;
@@ -16,6 +18,10 @@ typedef struct {
     int isOccupied;
     // Outros dados relevantes para a página, como o número do processo, podem ser adicionados aqui
 } Frame;
+
+PageTableEntry* tablePage;
+Frame* ram;
+int tableSize = 0;
 
 // Função para encontrar uma página na tabela de páginas
 int findPage(PageTableEntry* pageTable, int pageNumber, int tableSize) {
@@ -34,6 +40,49 @@ int replacePage(Frame* memory, int memorySize) {
     // Retorne o número do frame selecionado para substituição
 }
 
+void fifoAlg(int op, int address){
+    // encontrar pageNumber
+    int pageNumber;
+
+    int pageFound = findPageNumberInMemory(pageNumber);
+
+    if(pageFound == -1){
+        if(findPageNotOcuped(ram)){
+            // calcula a posição de inserção (processo mais antigo)
+            int subPosition;
+            
+            // converter page number in frameNumber
+            ram[subPosition].frameNumber = findPage(tablePage, pageNumber, tableSize);
+        }
+
+    }
+}
+
+void insertProcessInTablePage(int address){
+    // extrair o page number do address  
+    int pageNumber;
+
+    // verifica se a pagina ta na tabela
+    int pageFound = findPage(tablePage, pageNumber, tableSize);
+
+    // se a pagina não exstiver na tabela, inserir.
+    if(pageFound == -1){
+        // inserir pagina na tabela
+    }
+}
+
+void acessPageWithAlgoritm(int op, int address, int alg){
+    // encontra pagenumber
+    
+    // antes de acessar, colocar o processa na TB
+    insertProcessInTablePage(address);
+
+    // verifica se ta memoria
+    if(alg == 1){
+        fifoAlg(op, address);
+    }
+}
+
 // Função principal do simulador
 int main() {
     // Configurações do simulador
@@ -50,30 +99,31 @@ int main() {
     printf("%d\n", numFrames);
 
     // Alocação dinâmica da tabela de páginas e da memória RAM
-    PageTableEntry* pageTable = (PageTableEntry*)calloc(numPages, sizeof(PageTableEntry));
-    Frame* memory = (Frame*)calloc(numFrames, sizeof(Frame));
+    tablePage = (PageTableEntry*)calloc(numPages, sizeof(PageTableEntry));
+    ram = (Frame*)calloc(numFrames, sizeof(Frame));
 
     int totalPageFaults = 0;
 
     int op, address;
     
-    while (1) {
-        scanf("%d %x", &op, &address);
-        if(op == 0 && address == 0) break;
-        int pageNumber = address / pageSize;
-        int pageIndex = findPage(pageTable, pageNumber, numPages);
+    acessPageWithAlgoritm(op, address, algorithm);
+    // while (1) {
+    //     scanf("%d %x", &op, &address);
+    //     if(op == 0 && address == 0) break;
+    //     int pageNumber = address / pageSize;
+    //     int pageIndex = findPage(pageTable, pageNumber, numPages);
 
-        if (pageIndex == -1) {
-            printf("Page fault for address %x\n", address);
-            totalPageFaults++;
+    //     if (pageIndex == -1) {
+    //         printf("Page fault for address %x\n", address);
+    //         totalPageFaults++;
 
-            // Implemente aqui o tratamento da falta de página
-            // Você precisará substituir uma página na RAM e atualizar a tabela de páginas
-        }
+    //         // Implemente aqui o tratamento da falta de página
+    //         // Você precisará substituir uma página na RAM e atualizar a tabela de páginas
+    //     }
 
-        // A página está presente na RAM, faça o acesso normalmente
-        // Implemente aqui o restante do processamento do endereço
-    }
+    //     // A página está presente na RAM, faça o acesso normalmente
+    //     // Implemente aqui o restante do processamento do endereço
+    // }
 
     return 0;
 
